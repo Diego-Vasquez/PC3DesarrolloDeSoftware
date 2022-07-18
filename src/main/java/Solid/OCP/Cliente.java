a3a8d39c982eb0fc060ae18693175c745a82ec61
JoelJimenez <jjimenezch@uni.pe>
2022-07-18 04:40:55 -0500

    Avanzando el modulo 2

--- src/main/java/Solid/OCP/Cliente.java
+++ src/main/java/Solid/OCP/Cliente.java
@@ -1,5 +1,5 @@
 package Solid.OCP;
-/*
+
 import java.util.ArrayList;
 import java.util.List;
 public class Cliente {
@@ -20,8 +20,8 @@ public class Cliente {
 
         // Evalua las distinciones
 
-        DistinctionDecider scienceDistinctionDecider = new ScienceDistinctionDecider();
-        DistinctionDecider artsDistinctionDecider = new ArtsDistinctionDecider();
+        DistintionDecider scienceDistinctionDecider = new ScienceDistinctionDecider();
+        DistintionDecider artsDistinctionDecider = new ArtsDistinctionDecider();
         System.out.println("Distinciones:");
         for (Estudiante estudiante : CienciasEstudiantes) {
             scienceDistinctionDecider.evaluateDistinction(estudiante);
@@ -33,18 +33,18 @@ public class Cliente {
     }
 
     private static List<Estudiante> enrollScienceStudents() {
-        Estudiante Irene = new CienciaEstudiante("Irene", "R1", 81.5,"Ciencia de la computacion.");
-        Estudiante jessica = new CienciaEstudiante("Jessica", "R2", 72,"Fisica");
-        List<Estudiante> CienciasEstudiantes = new ArrayList<Estudiante>();
+        Estudiante Irene = new CienciaEstudiante("Irene", "R1", 81.5);
+        Estudiante jessica = new CienciaEstudiante("Jessica", "R2", 72);
+        List<Estudiante> CienciasEstudiantes = new ArrayList<>();
         CienciasEstudiantes.add(Irene);
         CienciasEstudiantes.add(jessica);
         return CienciasEstudiantes;
     }
 
     private static List<Estudiante> enrollArtsStudents() {
-        Estudiante chalo = new ArteEstudiante("Chalo", "R3", 71,"Historia");
-        Estudiante claudio = new ArteEstudiante("Claudio", "R4", 66.5,"Literatura");
-        List<Estudiante> ArtesEstudiantes = new ArrayList<Estudiante>();
+        Estudiante chalo = new ArteEstudiante("Chalo", "R3", 71);
+        Estudiante claudio = new ArteEstudiante("Claudio", "R4", 66.5);
+        List<Estudiante> ArtesEstudiantes = new ArrayList<>();
         ArtesEstudiantes.add(chalo);
         ArtesEstudiantes.add(claudio);
         return ArtesEstudiantes;
@@ -52,4 +52,3 @@ public class Cliente {
 
 
 }
-*/
